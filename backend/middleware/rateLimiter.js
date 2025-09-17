9046afa819cf473411e1c0e85ca3d9fe474e1dcb
juan <aswinjuanajk@gmail.com>
2025-09-17 19:21:27 +0700

    add react and rate limiter middleware

---
+++ backend/middleware/rateLimiter.js
@@ -0,0 +1,18 @@
+import ratelimit from "../src/config/upstash.js";
+
+const ratelimiter = async (req, res, next) => {
+  try {
+    const { success } = await ratelimit.limit("my-list-key");
+    if (!success) {
+      return res.status(400).json({
+        message: "too many requests, please try again later",
+      });
+    }
+    next();
+  } catch (error) {
+    res.status(500).json("Rate limit error", error);
+    next(error);
+  }
+};
+
+export default ratelimiter;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,17 @@
 import express from "express";
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
+import ratelimiter from "../middleware/rateLimiter.js";
 
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 5001;
 
 connectDB(); //connect to MongoDB lewat exportan db.js
 
 app.use(express.json()); // MIDDLEWARE ngebaca JSON
+app.use(ratelimiter);
 app.use("/api/notes", notesRoutes); // MIDDLEWARE untuk prefix di routes
 
-app.listen(port, () => {
-  console.log(`Server is running on ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
 });
